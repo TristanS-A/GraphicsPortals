fdc5fe3d7fc3e2010b1f2d427b22f56de1120403
DuskMoment <william.mansfield@mymail.champlain.edu>
2025-04-30 20:08:34 -0400

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- assignments/FINALPORTLALA/main.cpp
+++ assignments/FINALPORTLALA/main.cpp
@@ -60,7 +60,7 @@ public:
 
 	//function here becasue tristan is bad cringe and smells like carrots
 
-	glm::mat4 const ObliqueClippingMat(glm::mat4& const viewMatrix, glm::mat4& const projectionMatrix, const ew::Transform& trans)
+	glm::mat4& const ObliqueClippingMat(glm::mat4& const viewMatrix, glm::mat4& const projectionMatrix, const ew::Transform& trans)
 	{
 		float d = glm::length(trans.position);
 
@@ -103,15 +103,27 @@ ew::Mesh theCoolSphere;
 void RenderScene(ew::Shader& shader, ew::Shader& portalShader, GLuint tex, glm::mat4 view)
 {
 	glEnable(GL_CULL_FACE);
-	glCullFace(GL_FRONT);
 	glEnable(GL_DEPTH_TEST);
 
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, coolPortal.framebuffer.colorBuffer[0]);
+	glCullFace(GL_BACK);
+	glBindTexture(GL_TEXTURE_2D, tex);
 
-	// GFX Pass
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
+	shader.use();
+	shader.setMat4("camera_viewProj", view);
+	shader.setInt("_MainTex", 0);
+	shader.setMat4("_Model", coolSuzanneTransform.modelMatrix());
+	shader.setVec3("_ColorOffset", glm::vec3(0, 0, 1));
+	pCoolSuzanne->draw();
+
+	shader.setMat4("_Model", coolerSnazzySuzanneTransform.modelMatrix());
+	shader.setVec3("_ColorOffset", glm::vec3(1, 0, 0));
+	pCoolerSnazzySuzanne->draw();
+
+	shader.setMat4("_Model", recursiveSuzzaneTransform.modelMatrix());
+	shader.setVec3("_ColorOffset", glm::vec3(0, 1, 0));
+	pRecursiveSuzzane->draw();
+
+	glCullFace(GL_FRONT);
 
 	portalShader.use();
 
@@ -131,63 +143,170 @@ void RenderScene(ew::Shader& shader, ew::Shader& portalShader, GLuint tex, glm::
 	glBindTexture(GL_TEXTURE_2D, recursivePortal2.framebuffer.colorBuffer[0]);
 	portalShader.setMat4("_Model", recursivePortal2.regularPortalTransform.modelMatrix());
 	recursivePortal2.portalMesh.draw();
-
-	glCullFace(GL_BACK);
-	glBindTexture(GL_TEXTURE_2D, tex);
-
-	shader.use();
-	shader.setMat4("_Model", coolPortal.regularPortalTransform.modelMatrix());
-	shader.setMat4("camera_viewProj", view);
-	shader.setInt("_MainTex", 0);
-	shader.setMat4("_Model", coolSuzanneTransform.modelMatrix());
-	shader.setVec3("_ColorOffset", glm::vec3(0, 0, 1));
-	pCoolSuzanne->draw();
-
-	shader.setMat4("_Model", coolerSnazzySuzanneTransform.modelMatrix());
-	shader.setVec3("_ColorOffset", glm::vec3(1, 0, 0));
-	pCoolerSnazzySuzanne->draw();
-
-	shader.setMat4("_Model", recursiveSuzzaneTransform.modelMatrix());
-	shader.setVec3("_ColorOffset", glm::vec3(0, 1, 0));
-	pRecursiveSuzzane->draw();
 }
 
-void DrawRecursivePortals(glm::mat4 const& viewMatrix, glm::mat4 const& projMat, int maxRecursion, int currentRecursion, ew::Shader& sceneShader, ew::Shader& portalShader)
+void DrawRecursivePortals(glm::mat4& const viewMat, glm::mat4& const projMat, int maxRecursion, int currentRecursion, ew::Shader& sceneShader, ew::Shader& portalShader)
 {
-	for (int i = 0; i < 2; i++)
+
+	glBindFramebuffer(GL_FRAMEBUFFER, portals[1].framebuffer.fbo);
 	{
-		//Adds an offset to get the correct virtual camera rotation (not just the portals rotation)
-		ew::Transform linkedPTrans = portals[i].linkedPortal->regularPortalTransform;
-		linkedPTrans.rotation = glm::vec3(glm::eulerAngles(linkedPTrans.rotation) + portals[i].virtualCameraRotOffset);
-
-		//Get the virtual camera view matrix (rotates camera by current portal rotation and then linked portal rotation)
-		glm::mat4 destinationView =
-			camera.viewMatrix() * portals[i].regularPortalTransform.modelMatrix()
-			* glm::rotate(glm::mat4(1.0f), glm::radians(180.f), glm::vec3(0.0f, 1.0f, 0.0f))
-			* glm::inverse(linkedPTrans.modelMatrix());
-
-		if (currentRecursion == maxRecursion)
+		for (int i = 1; i < 2; i++)
 		{
-			glBindFramebuffer(GL_FRAMEBUFFER, portals[i].framebuffer.fbo);
+			glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+			glDepthMask(GL_FALSE);
+
+			// Disable depth test
+			glDisable(GL_DEPTH_TEST);
+
+			// Enable stencil test, to prevent drawing outside
+			// region of current portal depth
+			glEnable(GL_STENCIL_TEST);
+
+			// Fail stencil test when inside of outer portal
+			// (fail where we should be drawing the inner portal)
+			glStencilFunc(GL_NOTEQUAL, currentRecursion, 0xFF);
+
+			// Increment stencil value on stencil fail
+			// (on area of inner portal)
+			glStencilOp(GL_INCR, GL_KEEP, GL_KEEP);
+
+			// Enable (writing into) all stencil bits
+			glStencilMask(0xFF);
+			
+			portalShader.use();
+			portalShader.setMat4("camera_viewProj",projMat * viewMat);
+
+			portals[1].portalMesh.draw();
+
+			ew::Camera portal = camera;
+
+			/*glm::mat4 destinationView =
+				camera.viewMatrix() * p.regularPortalTransform.modelMatrix()
+				* glm::rotate(glm::mat4(1.0f), glm::radians(180.f), glm::vec3(0.0f, 1.0f, 0.0f))
+					* glm::inverse(p.linkedPortal->regularPortalTransform.modelMatrix());*/
+
+					// Translates camera position and target in relation to linked portal. This only works for specific portal rotations
+
+			auto p = portals[1];
+			glm::vec3 toPortal = p.regularPortalTransform.position - camera.position;
+			glm::vec3 translatedTarget = p.regularPortalTransform.position - camera.target;
+			portal.position = p.linkedPortal->regularPortalTransform.position - toPortal;
+			portal.target = p.linkedPortal->regularPortalTransform.position - translatedTarget;
+
+			//Adds an offset to get the correct virtual camera rotation (not just the portals rotation)
+			ew::Transform linkedPTrans = p.linkedPortal->regularPortalTransform;
+			linkedPTrans.rotation = glm::vec3(glm::eulerAngles(linkedPTrans.rotation) + p.virtualCameraRotOffset);
+
+			//Get the virtual camera view matrix (rotates camera by current portal rotation and then linked portal rotation)
+			glm::mat4 destinationView =
+				camera.viewMatrix() * p.regularPortalTransform.modelMatrix()
+				* glm::rotate(glm::mat4(1.0f), glm::radians(180.f), glm::vec3(0.0f, 1.0f, 0.0f))
+				* glm::inverse(linkedPTrans.modelMatrix());
+
+			// Base case, render inside of inner portal
+			if (currentRecursion == maxRecursion)
 			{
+				// Enable color and depth drawing
+				glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+				glDepthMask(GL_TRUE);
+
+				// Clear the depth buffer so we don't interfere with stuff
+				// outside of this inner portal
+				glClear(GL_DEPTH_BUFFER_BIT);
+
+				// Enable the depth test
+				// So the stuff we render here is rendered correctly
 				glEnable(GL_DEPTH_TEST);
-				RenderScene(sceneShader, portalShader, rockNormal, projMat * destinationView);
 
-			}glBindFramebuffer(GL_FRAMEBUFFER, 0);
-			//return;
+				// Enable stencil test
+				// So we can limit drawing inside of the inner portal
+				glEnable(GL_STENCIL_TEST);
+
+				// Disable drawing into stencil buffer
+				glStencilMask(0x00);
+
+				// Draw only where stencil value == recursionLevel + 1
+				// which is where we just drew the new portal
+				glStencilFunc(GL_EQUAL, currentRecursion + 1, 0xFF);
+
+				// Draw scene objects with destView, limited to stencil buffer
+				// use an edited projection matrix to set the near plane to the portal plane
+				
+				//drawNonPortals(destView, projMat);
+				RenderScene(sceneShader, portalShader, rockNormal, destinationView);
+			}
+			else
+			{
+				// Recursion case
+				// Pass our new view matrix and the clipped projection matrix (see above)
+				DrawRecursivePortals(destinationView, portals[1].ObliqueClippingMat(viewMat, projMat, portals[1].regularPortalTransform), maxRecursion, currentRecursion + 1, sceneShader, portalShader);
+			}
+
+			// Disable color and depth drawing
+			glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+			glDepthMask(GL_FALSE);
+
+			// Enable stencil test and stencil drawing
+			glEnable(GL_STENCIL_TEST);
+			glStencilMask(0xFF);
+
+			// Fail stencil test when inside of our newly rendered
+			// inner portal
+			glStencilFunc(GL_NOTEQUAL, currentRecursion + 1, 0xFF);
+
+			// Decrement stencil value on stencil fail
+			// This resets the incremented values to what they were before,
+			// eventually ending up with a stencil buffer full of zero's again
+			// after the last (outer) step.
+			glStencilOp(GL_DECR, GL_KEEP, GL_KEEP);
+
+			// Draw portal into stencil buffer
+			portalShader.use();
+			portalShader.setMat4("camera_viewProj", projMat * viewMat);
+			portals[1].portalMesh.draw();
+
 		}
-		else
-		{
-			DrawRecursivePortals(destinationView, projMat, maxRecursion, currentRecursion + 1, sceneShader, portalShader);
-		}
 
-		glBindFramebuffer(GL_FRAMEBUFFER, portals[i].framebuffer.fbo);
-		{
-			glEnable(GL_DEPTH_TEST);
-			RenderScene(sceneShader, portalShader, rockNormal, projMat * destinationView);
+		// Disable the stencil test and stencil writing
+		glDisable(GL_STENCIL_TEST);
+		glStencilMask(0x00);
 
-		}glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		// Disable color writing
+		glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+
+		// Enable the depth test, and depth writing.
+		glEnable(GL_DEPTH_TEST);
+		glDepthMask(GL_TRUE);
+
+		// Make sure we always write the data into the buffer
+		glDepthFunc(GL_ALWAYS);
+
+		// Clear the depth buffer
+		glClear(GL_DEPTH_BUFFER_BIT);
+
+		// Reset the depth function to the default
+		glDepthFunc(GL_LESS);
+
+		// Enable stencil test and disable writing to stencil buffer
+		glEnable(GL_STENCIL_TEST);
+		glStencilMask(0x00);
+
+		// Draw at stencil >= recursionlevel
+		// which is at the current level or higher (more to the inside)
+		// This basically prevents drawing on the outside of this level.
+		glStencilFunc(GL_LEQUAL, currentRecursion, 0xFF);
+
+		// Enable color and depth drawing again
+		glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+		glDepthMask(GL_TRUE);
+
+		// And enable the depth test
+		glEnable(GL_DEPTH_TEST);
+
+
+		RenderScene(sceneShader, portalShader, rockNormal, viewMat * projMat);
 	}
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 void RenderPortalView(Portal& p, ew::Shader& sceneShader, ew::Shader& portalShader)
@@ -216,7 +335,7 @@ void RenderPortalView(Portal& p, ew::Shader& sceneShader, ew::Shader& portalShad
 		* glm::rotate(glm::mat4(1.0f), glm::radians(180.f), glm::vec3(0.0f, 1.0f, 0.0f))
 		* glm::inverse(linkedPTrans.modelMatrix());
 
-	DrawRecursivePortals(camera.viewMatrix(), camera.projectionMatrix(), 2, 0, sceneShader, portalShader);
+	//DrawRecursivePortals(camera.viewMatrix(), camera.projectionMatrix(), 2, 0, sceneShader, portalShader);
 
 	////A reminder that late night quick maths are not always going to be scalable :(
 	/*glm::quat roation = glm::vec3(glm::radians(-90.f), 0, 0);
@@ -300,9 +419,43 @@ int main() {
 		//RENDER
 		camController.move(window, &camera, deltaTime);
 
-		//RenderPortalView(coolPortal, defaultLit, portalView);
-		DrawRecursivePortals(camera.viewMatrix(), camera.projectionMatrix(), 5, 0, defaultLit, portalView);
+		/*glBindFramebuffer(GL_FRAMEBUFFER, 0);*/
+
+		// GFX Pass
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
+
+		//for (int i = 0; i < 2; i++)
+		//{
+		//	glBindFramebuffer(GL_FRAMEBUFFER, portals[i].framebuffer.fbo);
+
+		//	// GFX Pass
+		//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//	glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
+		//}
+		
+
+		//Adds an offset to get the correct virtual camera rotation (not just the portals rotation)
+		ew::Transform linkedPTrans = portals[1].linkedPortal->regularPortalTransform;
+		linkedPTrans.rotation = glm::vec3(glm::eulerAngles(linkedPTrans.rotation) + portals[1].linkedPortal->virtualCameraRotOffset);
+
+		//Get the virtual camera view matrix (rotates camera by current portal rotation and then linked portal rotation)
+		glm::mat4 destinationView =
+			camera.viewMatrix() * portals[1].regularPortalTransform.modelMatrix()
+			* glm::rotate(glm::mat4(1.0f), glm::radians(180.f), glm::vec3(0.0f, 1.0f, 0.0f))
+			* glm::inverse(linkedPTrans.modelMatrix());
+
+		
+		//base case
+		//RenderScene(defaultLit, portalView, rockNormal, camera.projectionMatrix() * destinationView);
+
+		//glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
 		RenderScene(defaultLit, portalView, rockNormal, camera.projectionMatrix() * camera.viewMatrix());
+
+		DrawRecursivePortals(camera.viewMatrix(), camera.projectionMatrix(), 5, 0, defaultLit, portalView);
+
+
+	
 
 
 		drawUI();
